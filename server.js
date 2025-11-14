9213f3ce2f289aae2eb159b20a2079a3aa78e3e8
Dev Ask <techdevask@gmail.com>
2025-11-14 09:07:49 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+// ==================== server.js ====================
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import code from './pair.js';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __path = process.cwd();
+const PORT = process.env.PORT || 8000;
+
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/code', code);
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+export default app;
